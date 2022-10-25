a884639c5eb40f2afffa80d4b763b5e23719c3f5
morelk19 <morelkhristian@gmail.com>
2022-10-25 12:51:41 -0400

    completed assignment

---
+++ main.js
@@ -0,0 +1,24 @@
+let para = document.querySelector("p");
+para.style.color = 'lightblue';
+
+let heading = document.querySelector('h1');
+heading.style.fontSize = "48px";
+
+para.innerText = 'Lorem ipsum dolor amet viral meh selfies drinking vinegar, intelligentsia poke flannel twee paleo enamel pin cray. Banjo celiac crucifix, kickstarter la croix air plant jianbing hashtag vinyl hell of man bun selvage schlitz banh mi. Tacos hella raclette quinoa blog, williamsburg adaptogen tbh. Hexagon af stumptown lumbersexual synth gentrify quinoa enamel pin celiac master cleanse. Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami distillery austin hashtag. Food truck synth wayfarers, street art banh mi actually authentic. Bitters tousled tattooed vegan neutra pug hell of fixie chia unicorn letterpress.'
+
+let item13 = document.querySelector("#item-13");
+item13.style.opacity = "0.5";
+
+let item3 = document.querySelector("#item-3");
+item3.innerText= "I say, \"Hi!\"";
+
+let img = document.querySelector("img");
+img.src = 'https://www.zeldadungeon.net/wiki/images/thumb/f/fd/Link.png/400px-Link.png'
+img.style.height = "300px";
+
+let img2 = document.querySelector("#img2");
+img2.style.height = "300px";
+
+let item16 = document.querySelector("#item-16");
+item16.className= "item";
+item16.innerText = 'Won\'t get fooled again.';
